e9c606fc0a93ffaecfb65be9e8f8a3d932956520
glabular <glu.max06@gmail.com>
2025-08-02 15:25:00 +0200

    Annotated SpacesPagination class properties

--- Anytype.NET/Models/SpacesPagination.cs
+++ Anytype.NET/Models/SpacesPagination.cs
@@ -1,12 +1,18 @@
-﻿namespace Anytype.NET.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Anytype.NET.Models;
 
 public class SpacesPagination
 {
+    [JsonPropertyName("total")]
     public int Total { get; set; }
 
+    [JsonPropertyName("offset")]
     public int Offset { get; set; }
 
+    [JsonPropertyName("limit")]
     public int Limit { get; set; }
 
+    [JsonPropertyName("has_more")]
     public bool HasMore { get; set; }
 }
