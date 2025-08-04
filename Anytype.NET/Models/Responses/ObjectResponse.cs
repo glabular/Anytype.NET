436564a823c0ed4d1e92274bd5287283649131cb
glabular <glu.max06@gmail.com>
2025-08-04 14:01:39 +0200

    Updated docs

--- Anytype.NET/Models/Responses/ObjectResponse.cs
+++ Anytype.NET/Models/Responses/ObjectResponse.cs
@@ -1,6 +1,12 @@
 ﻿namespace Anytype.NET.Models.Responses;
 
+/// <summary>
+/// Represents a standard response wrapper containing an <see cref="AnyObject"/> returned by the Anytype API.
+/// </summary>
 public class ObjectResponse
 {
+    /// <summary>
+    /// The <see cref="AnyObject"/> contained within the response body.
+    /// </summary>
     public AnyObject Object { get; set; }
 }
