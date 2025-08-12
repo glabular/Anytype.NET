001319c0a18e41818abe34dd056ffc3c4454e6ec
glabular <glu.max06@gmail.com>
2025-08-12 09:32:26 +0200

    Updated docs

--- Anytype.NET/Models/AnyObject.cs
+++ Anytype.NET/Models/AnyObject.cs
@@ -5,25 +5,58 @@
 /// </summary>
 public class AnyObject
 {
+    /// <summary>
+    /// The data model of the object.
+    /// </summary>
     public string Object { get; set; }
 
+    /// <summary>
+    /// The id of the object.
+    /// </summary>
     public string Id { get; set; }
 
+    /// <summary>
+    /// The name of the object.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// The icon of the object, or null if the object has no icon
+    /// </summary>
     public Icon Icon { get; set; }
 
+    /// <summary>
+    /// Whether the object is archived.
+    /// </summary>
     public bool Archived { get; set; }
 
+    /// <summary>
+    /// The id of the space the object is in.
+    /// </summary>
     public string SpaceId { get; set; }
 
+    /// <summary>
+    /// The snippet of the object, especially important for notes as they don't have a name.
+    /// </summary>
     public string Snippet { get; set; }
 
+    /// <summary>
+    /// The layout of the object.
+    /// </summary>
     public string Layout { get; set; }
 
+    /// <summary>
+    /// The type of the object, or null if the type has been deleted.
+    /// </summary>
     public ApiObjectType Type { get; set; }
 
+    /// <summary>
+    /// The properties of the object.
+    /// </summary>
     public List<Property> Properties { get; set; }
 
+    /// <summary>
+    /// The markdown body of the object.
+    /// </summary>
     public string Markdown { get; set; }
 }
--- Anytype.NET/Models/ApiObjectType.cs
+++ Anytype.NET/Models/ApiObjectType.cs
@@ -2,22 +2,49 @@
 
 public class ApiObjectType
 {
+    /// <summary>
+    /// The data model of the object.
+    /// </summary>
     public string Object { get; set; }
 
+    /// <summary>
+    /// The id of the type (which is unique across spaces).
+    /// </summary>
     public string Id { get; set; }
 
+    /// <summary>
+    /// The key of the type (can be the same across spaces for known types).
+    /// </summary>
     public string Key { get; set; }
 
+    /// <summary>
+    /// The name of the type.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// The plural name of the type.
+    /// </summary>
     public string PluralName { get; set; }
 
+    /// <summary>
+    /// The icon of the object, or null if the object has no icon.
+    /// </summary>
     public Icon Icon { get; set; }
 
+    /// <summary>
+    /// Whether the type is archived.
+    /// </summary>
     public bool Archived { get; set; }
 
+    /// <summary>
+    /// The layout of the object.
+    /// </summary>
     public string Layout { get; set; }
 
     // TODO: not sure about this type, it could be a list.
+    /// <summary>
+    /// The properties linked to the type.
+    /// </summary>
     public object Properties { get; set; }
 }
