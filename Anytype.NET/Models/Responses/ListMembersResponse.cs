4ca0736083cce377ec3e371434f034fb164d4c2e
glabular <glu.max06@gmail.com>
2025-08-13 13:33:12 +0200

    Added ListMembersResponse model

---
+++ Anytype.NET/Models/Responses/ListMembersResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Anytype.NET.Models.Responses;
+
+public class ListMembersResponse
+{
+    [JsonPropertyName("data")]
+    public List<AnyMember> Members { get; set; }
+
+    [JsonPropertyName("pagination")]
+    public PaginationMetadata Pagination { get; set; }
+}
