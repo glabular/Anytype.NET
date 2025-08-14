3444bf8baab89a0de8c362f9b924d24bf4f74679
glabular <glu.max06@gmail.com>
2025-08-14 12:06:23 +0200

    Added tags client

---
+++ Anytype.NET/Internal/TagsClient.cs
@@ -0,0 +1,11 @@
+﻿using Anytype.NET.Models;
+using Anytype.NET.Models.Requests;
+using Anytype.NET.Models.Responses;
+using System.Text.Json;
+
+namespace Anytype.NET.Internal;
+
+public sealed class TagsClient :ClientBase
+{
+    public TagsClient(string apiKey) : base(apiKey) { }
+}
