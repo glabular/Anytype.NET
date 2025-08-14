11bdd87606aa7ab78dcf27b91567b12419d851a5
glabular <glu.max06@gmail.com>
2025-08-14 15:33:09 +0200

    Added properties client

---
+++ Anytype.NET/Internal/PropertiesClient.cs
@@ -0,0 +1,11 @@
+﻿using Anytype.NET.Models;
+using Anytype.NET.Models.Requests;
+using Anytype.NET.Models.Responses;
+using System.Text.Json;
+
+namespace Anytype.NET.Internal;
+
+public sealed class PropertiesClient : ClientBase
+{
+    public PropertiesClient(string apiKey) : base(apiKey) { }
+}
