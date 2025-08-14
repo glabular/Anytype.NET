bf63e9c08fcfc1e68827476a636ba2bcb549e102
glabular <glu.max06@gmail.com>
2025-08-14 21:35:36 +0200

    Added types client

---
+++ Anytype.NET/Internal/TypesClient.cs
@@ -0,0 +1,14 @@
+﻿using Anytype.NET.Models;
+using Anytype.NET.Models.Requests;
+using Anytype.NET.Models.Responses;
+using System.Text.Json;
+
+namespace Anytype.NET.Internal;
+
+/// <summary>
+/// Provides methods to interact with Anytype types.
+/// </summary>
+public sealed class TypesClient : ClientBase
+{
+    public TypesClient(string apiKey) : base(apiKey) { }
+}
