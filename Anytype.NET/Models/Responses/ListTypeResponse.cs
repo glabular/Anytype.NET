994079adc85182556c1312d5711352670eb9af1e
glabular <glu.max06@gmail.com>
2025-08-15 00:32:29 +0200

    Added ListTypeResponse class

---
+++ Anytype.NET/Models/Responses/ListTypeResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Anytype.NET.Models.Responses;
+
+public sealed class ListTypeResponse
+{
+    [JsonPropertyName("data")]
+    public List<AnyType> Data { get; set; }
+
+    [JsonPropertyName("pagination")]
+    public PaginationMetadata Pagination { get; set; }
+}
