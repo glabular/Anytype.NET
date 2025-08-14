bf4a0a9d85b8e1cb0118d6793c66220badd00e62
glabular <glu.max06@gmail.com>
2025-08-14 12:03:37 +0200

    Added ListTagsResponse model

---
+++ Anytype.NET/Models/Responses/ListTagsResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Anytype.NET.Models.Responses;
+
+/// <summary>
+/// Represents the response returned from the Anytype API when requesting a list of tags.
+/// </summary>
+public sealed class ListTagsResponse
+{
+    /// <summary>
+    /// The list of tag items in the current result set.
+    /// </summary>
+    [JsonPropertyName("data")]
+    public List<AnyTag> Tags { get; set; }
+
+    /// <summary>
+    /// The pagination metadata for the response.
+    /// </summary>
+    public PaginationMetadata Pagination { get; set; }
+}
