aa5c709cfbc8eb4e60b512bcaec38640843c5b2c
glabular <glu.max06@gmail.com>
2025-08-14 12:22:09 +0200

    Implemented tags listing

--- Anytype.NET/Internal/TagsClient.cs
+++ Anytype.NET/Internal/TagsClient.cs
@@ -5,7 +5,27 @@ using System.Text.Json;
 
 namespace Anytype.NET.Internal;
 
-public sealed class TagsClient :ClientBase
+public sealed class TagsClient : ClientBase
 {
     public TagsClient(string apiKey) : base(apiKey) { }
+
+    public async Task<ListTagsResponse> ListTagsAsync(string spaceId, string propertyId)
+    {
+        if (string.IsNullOrWhiteSpace(spaceId))
+        {
+            throw new ArgumentNullException(nameof(spaceId));
+        }
+
+        if (string.IsNullOrWhiteSpace(propertyId))
+        {
+            throw new ArgumentNullException(nameof(propertyId));
+        }
+
+        var relativeUrl = $"/v1/spaces{spaceId}/properties/{propertyId}/tags";
+
+        var response = await GetAsync<ListTagsResponse>(relativeUrl)
+            ?? throw new InvalidOperationException("The API returned an empty response.");
+
+        return response;
+    }
 }
