fbf28a2a58ffc592973580912c61dc799ea3b3bd
glabular <glu.max06@gmail.com>
2025-08-15 04:37:11 +0200

    Added payload for type creation

---
+++ Anytype.NET/Models/Requests/CreateTypePropertyRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Anytype.NET.Models.Requests;
+
+public sealed class CreateTypePropertyRequest
+{
+    /// <summary>
+    /// The format of the property used for filtering.
+    /// </summary>
+    public string Format { get; set; }
+
+    /// <summary>
+    /// The key of the property.
+    /// </summary>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// The name of the property.
+    /// </summary>
+    public string Name { get; set; }
+}
---
+++ Anytype.NET/Models/Requests/CreateTypeRequest.cs
@@ -0,0 +1,36 @@
+﻿namespace Anytype.NET.Models.Requests;
+
+public sealed class CreateTypeRequest
+{
+    /// <summary>
+    /// The icon of the type (nullable).
+    /// </summary>
+    public Icon? Icon { get; set; }
+
+    /// <summary>
+    /// The key of the type.
+    /// </summary>
+    /// <remarks>Should always be snake_case, otherwise it will be converted to snake_case.</remarks>
+    public string Key { get; set; }
+
+    /// <summary>
+    /// The layout of the type. Possible values: basic, profile, action, note.
+    /// </summary>
+    public string Layout { get; set; }
+
+    /// <summary>
+    /// The name of the type.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The plural name of the type.
+    /// </summary>
+    public string PluralName { get; set; }
+
+    /// <summary>
+    /// The list of properties linked to the type.
+    /// ⚠ Experimental and subject to change.
+    /// </summary>
+    public List<CreateTypePropertyRequest> Properties { get; set; }
+}
