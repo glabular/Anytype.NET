e57403a8934b40f290634068942c356bc2e2e5b2
glabular <glu.max06@gmail.com>
2025-08-15 04:46:51 +0200

    Added CreateTypeResponse model

---
+++ Anytype.NET/Models/Responses/CreateTypeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Anytype.NET.Models.Responses;
+
+public sealed class CreateTypeResponse
+{
+    /// <summary>
+    /// The created type object.
+    /// </summary>
+    public AnyType? Type { get; set; }
+}
