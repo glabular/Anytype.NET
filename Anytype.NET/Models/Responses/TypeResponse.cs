74e88beeeb7a560174310c1af8383a8ecf25d5e4
glabular <glu.max06@gmail.com>
2025-08-15 15:59:49 +0200

    Added type response model

---
+++ Anytype.NET/Models/Responses/TypeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Anytype.NET.Models.Responses;
+
+public sealed class TypeResponse
+{
+    /// <summary>
+    /// The <see cref="AnyType"/> contained within the response body.
+    /// </summary>
+    public AnyType Type { get; set; }
+}
