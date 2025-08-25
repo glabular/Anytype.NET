39330a87053c4390345d6b7aa14707b96f1b731b
glabular <glu.max06@gmail.com>
2025-08-25 14:41:55 +0200

    Replaced API key retrieval with environment variable

--- DevConsole/Program.cs
+++ DevConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Anytype.NET;
-using Microsoft.Extensions.Configuration;
 
 namespace DevConsole;
 
@@ -7,24 +6,16 @@ internal class Program
 {
     static async Task Main()
     {
-        var apiKey = GetApiKeyFromConfig();
+        var apiKey = Environment.GetEnvironmentVariable("ANYTYPE_API_TESTING_KEY");
+
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            throw new InvalidOperationException("Set ANYTYPE_API_TESTING_KEY environment variable first.");
+        }
+
         var client = new AnytypeClient(apiKey);
         var demo = new DemoRunner(client);
 
         await demo.RunAsync();
-    }    
-
-    private static string GetApiKeyFromConfig()
-    {
-        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
-
-        var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? string.Empty;
-
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
-            .Build();
-
-        return config["ApiKey"] ?? string.Empty;
-    }
+    } 
 }
