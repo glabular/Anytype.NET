dff8ba985afe4bcfbdc8f6a7a25a5c9546cc1afe
glabular <glu.max06@gmail.com>
2025-09-02 13:46:43 +0200

    Added static AnytypeApiVersions class

---
+++ Anytype.NET/Constants/AnytypeApiVersions.cs
@@ -0,0 +1,13 @@
+﻿namespace Anytype.NET.Constants;
+
+public static class AnytypeApiVersions
+{
+    public const string V20250520 = "2025-05-20";
+    public const string V20250422 = "2025-04-22";
+    public const string V20250317 = "2025-03-17";
+
+    /// <summary>
+    /// Returns the latest known API version.
+    /// </summary>
+    public static string GetLatest() => V20250520;
+}
